aa4423abdcb176a64f5b6d387e8db29c3662abc7
dan <dan@dancrn.com>
2014-11-28 16:15:37 +0000

    updated readme to reflect reality

--- README.md
+++ README.md
@@ -6,22 +6,21 @@ usage
 =====
 lms can be used to list available modules, load modules, and unload modules. nothing really happens, if i'm honest. some environment variables are exported (and are not persistent), in particular, the `LD_LIBRARY_PATH`, `PATH`, and `C_INCLUDE_PATH` variables. that's kind of it, really.
 
-    $ lms list
+    $ module list
 
 will list available modules.
 
-    $ lms which gfortran
+    $ module which gfortran
 
 this will list what modules provide the `gfortran` binary. note, `which` will search through each module to see which ones provide the *binary* requested. this is not a search of module names. to load that module, run
 
-    $ lms load languages,gcc,4.9.1
+    $ module load languages/gcc/4.9.1
 
 then `gfortran` should work. note that the above and
     
-    $ lms load gcc
+    $ module load gcc
 
 are equivilent, provided there is no ambiguity in only specifying the latter. finally, unloading modules is achieved with
 
-    $ lms unload gcc
+    $ module unload gcc
 
-if you attempt to load two modules which provide the same binary, an error will be returned, but this can be over-ridden by specifying `--force`.
--- src/main.c
+++ src/main.c
@@ -18,6 +18,8 @@ main
     return lms_list_available();
   else if (!strcmp(args[1], "list"))
     return lms_list_loaded();
+  else if (!strcmp(args[1], "help"))
+    return lms_help(), 0;
 
   //if we only have 2 args, we can't have any useful action
   if (2 == num_args)
@@ -29,9 +31,9 @@ main
 
   if (!strcmp(args[1], "which"))
     return lms_list_which(num_args-2, &args[2]);
-  else if (!strcmp(args[1], "load"))
+  else if (!strcmp(args[1], "load") || !strcmp(args[1], "add"))
     return lms_module_load(num_args-2, &args[2]);
-  else if (!strcmp(args[1], "unload"))
+  else if (!strcmp(args[1], "unload") || !strcmp(args[1], "rm") || !strcmp(args[1], "del"))
     return lms_module_unload(num_args-2, &args[2]);
 
   fprintf(stderr, "Error: Unknown action \"%s\"\n", args[1]);
@@ -43,5 +45,23 @@ void
 lms_help
 (void)
 {
-  printf("print some help here\n");
+  char *help_str = {
+    "Usage:\n"
+    "  module [action]\n"
+    "\n"
+    "Where action is one of:\n"
+    "  avail            - Lists available modules\n"
+    "  list             - Lists loaded modules\n"
+    "  which [binary]   - Lists module(s) that provide [binary]\n"
+    "  load [module]    - Loads [module] (fully qualified name is not needed)\n"
+    "  unload [module]  - Removes a module (if loaded)\n"
+    "\n"
+    "Aliases for load:\n"
+    "  add\n"
+    "\n"
+    "Aliases for unload:\n"
+    "  del, rm\n"
+  };
+
+  fprintf(stdout, "%s\n", help_str);
 }
